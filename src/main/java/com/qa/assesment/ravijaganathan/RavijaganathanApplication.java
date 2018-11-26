239f3b9191e52b6b7360dbc08d63db23d1693a1b
htanivar <37423501+htanivar@users.noreply.github.com>
2018-11-26 13:25:48 +0000

    Spring Boot Initial Commit

---
+++ src/main/java/com/qa/assesment/ravijaganathan/RavijaganathanApplication.java
@@ -0,0 +1,12 @@
+package com.qa.assesment.ravijaganathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavijaganathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavijaganathanApplication.class, args);
+	}
+}
